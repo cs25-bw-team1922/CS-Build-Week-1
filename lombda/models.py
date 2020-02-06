df7b4a43cabc5c49da50de78e1eb27f22ef0a164
danpatrickoneill <dan47573@gmail.com>
2020-02-06 14:17:37 -0500

    Removed custom User model

--- lombda/models.py
+++ lombda/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    first_name = models.TextField(max_length=30)
-    last_name = models.TextField(max_length=30)
-    user_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=254)
+# class User(models.Model):
+#     first_name = models.TextField(max_length=30)
+#     last_name = models.TextField(max_length=30)
+#     user_name = models.CharField(max_length=30)
+#     email = models.EmailField(max_length=254)
